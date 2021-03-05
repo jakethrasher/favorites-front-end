512bbdeef6c667d48f94f18ff91378ac9d4394b0
Jake Thrasher <mjakethrasher@gmail.com>
2021-03-04 17:46:37 -0800

    homeage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import { serialize } from 'superagent'
 
 export default class HomePage extends Component {
     render() {
+        const home={
+            fontSize:'3em',
+            display: 'flex',
+            flexWrap:'wrap'
+        } 
+        const text={
+            margin:'0 auto'
+        }
         return (
             <div>
-                <div>
-                    <h1>HOMEPAGE</h1>
+                <div style={home}>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
+                    <h1 style={text}>HOMEPAGE</h1>
                 </div>
             </div>
         )
     }
 }
+                    
+                   
